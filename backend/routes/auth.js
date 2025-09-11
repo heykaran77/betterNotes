5653dfd30277218cd139808d61ed51eb4b649740
Karan Singh <devkaran.codes@gmail.com>
2025-09-11 22:12:51 +0530

    modify the auth route to auth/createUser

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,26 +4,32 @@ import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
+//create new user
 router.post(
-  "/",
+  "/createUser",
   [
-    body("name", "Enter a valid Name").isLength({ min: 5 }),
+    body("name", "Enter a valid Name").isLength({ min: 3 }),
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Enter a valid Password").isLength({ min: 5 }),
   ],
   async (req, res) => {
     const result = validationResult(req);
+    // If Validation unsuccessful
     if (!result.isEmpty()) {
       return res.status(400).json({ errors: result.array() });
     }
     try {
-      await User.create({
+      //Using User Model to CREATE user
+      let user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-      }).then((user) => res.json(user));
+      });
+      res.json(user);
     } catch (e) {
-      console.log(e);
+      if (e.code === 11000) {
+        return res.status(400).json({ message: "Email already exists" });
+      }
       res.status(500).json({ message: e.message });
     }
   }
