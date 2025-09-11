55c9f27019c4f2f2e2499aaa1d17a18a0c762508
Karan Singh <devkaran.codes@gmail.com>
2025-09-11 21:38:43 +0530

    add validations to the userData

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const mongooseURI = "mongodb://localhost:27017/";
+const mongooseURI = "mongodb://localhost:27017/betterNotesDB";
 
 const connectToMongo = () => {
   mongoose
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,31 @@
 import express from "express";
 import User from "../models/User.js";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
-
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid Name").isLength({ min: 5 }),
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Enter a valid Password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() });
+    }
+    try {
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then((user) => res.json(user));
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: e.message });
+    }
+  }
+);
 export default router;
