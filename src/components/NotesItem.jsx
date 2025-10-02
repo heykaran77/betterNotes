eb63641371829783ccdb8d52f1290a9376abb247
Karan Singh <devkaran.codes@gmail.com>
2025-10-02 16:37:52 +0530

    minor bugs

--- src/components/NotesItem.jsx
+++ src/components/NotesItem.jsx
@@ -8,12 +8,14 @@ const NotesItem = ({ note, updateNote }) => {
   const { deleteNote } = useContext(noteContext);
   return (
     <div className="border-2 border-dashed px-4 py-6 rounded-sm">
-      <h2 className="font-bold text-center text-xl">{title}</h2>
-      <p className="mt-4">{description}</p>
-      <Badge variant="default" className="rounded-lg font-light">
-        {tag}
-      </Badge>{" "}
-      | <span>{new Date(date).toLocaleString()}</span>
+      <div className="flex justify-between items-center">
+        <h2 className="font-bold text-center text-xl">{title}</h2>
+        <Badge variant="default" className="rounded-lg font-medium">
+          {tag}
+        </Badge>
+      </div>
+      <p className="mt-4">{description}</p>{" "}
+      <span>{new Date(date).toLocaleString()}</span>
       <div className="flex gap-4 justify-end mt-4">
         <Trash
           className="cursor-pointer text-red-500"
