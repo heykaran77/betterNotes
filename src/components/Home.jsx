001b45e244c301f7f5a3f1f178942a8b8b0539ef
Karan Singh <devkaran.codes@gmail.com>
2025-11-06 00:57:31 +0530

    testimonials section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import Features from "./Features";
+import Testimonials from "./Testimonials";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Features />
+      <Testimonials />
     </>
   );
 };
